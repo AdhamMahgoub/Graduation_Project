d1c0bbc9a7254a9030716a4b2430cf6ea4b7f68d
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-02-28 08:30:32 +0200

    understood the addresses and arguments in the program

---
+++ Low_Level_MC/Mastering_I2C/2. Understood_Addresses_and_Arguments/I2C/I2C/Debug/EXTI_program.d
@@ -0,0 +1,13 @@
+EXTI_program.d EXTI_program.o: .././EXTI_program.c .././STD_TYPES.h \
+ .././BIT_MATH.h .././EXTI_interface.h .././EXTI_private.h \
+ .././EXTI_config.h
+
+.././STD_TYPES.h:
+
+.././BIT_MATH.h:
+
+.././EXTI_interface.h:
+
+.././EXTI_private.h:
+
+.././EXTI_config.h:
---
+++ Low_Level_MC/Mastering_I2C/2. Understood_Addresses_and_Arguments/I2C/I2C/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+DIO_program.c
+
+EXTI_program.c
+
+I2C_program.c
+
+LCD_program.c
+
+main.c
+

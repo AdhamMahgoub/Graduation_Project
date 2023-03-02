5cb0ed5548e06f601dab4475e7bbba8fc958a812
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-03-02 18:17:33 +0200

    removed the un-necessary files

--- Low_Level_MC/1. LCD_using_I2C/2. Enhanced_The_Download_Drivers/LCD_I2C/Debug/makedep.mk
+++ Low_Level_MC/1. LCD_using_I2C/2. Enhanced_The_Download_Drivers/LCD_I2C/Debug/makedep.mk
@@ -2,8 +2,6 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-DIO_program.c
-
 liquid_crystal_i2c_master.c
 
 liquid_crystal_i2c.c

5fdcab5d776ecad7d6765600f7acd64a665afce0
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-03-06 16:36:36 +0200

    modified some things in the I2C + MPU6050 -- but not working yet -- need to adjust the duplicate I2C drivers

--- Low_Level_MC/# I2C_LCD + MPU6050/LCD_I2C/HAL/MPU6050/MPU6050.c
+++ Low_Level_MC/# I2C_LCD + MPU6050/LCD_I2C/HAL/MPU6050/MPU6050.c
@@ -1,7 +1,6 @@
 #include "MPU6050_private.h"
 #include "MPU6050_I2C_Master.h"
 
-
 float Acc_x,Acc_y,Acc_z,Temperature,Gyro_x,Gyro_y,Gyro_z;
 
 void MPU6050_Init()										/* Gyro initialization function */

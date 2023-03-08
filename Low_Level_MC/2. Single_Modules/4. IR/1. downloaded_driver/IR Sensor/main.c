31cbf74aabc7b5f1eee6d86cd4153ae00b34b971
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-03-08 04:39:02 +0200

    added the downloaded driver

---
+++ Low_Level_MC/2. Single_Modules/4. IR/1. downloaded_driver/IR Sensor/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 08-Mar-23 4:37:42 AM
+ *  Author: adham
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}

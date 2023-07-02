2d615ee650160adb0b348092853eb6847042fa95
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-07-03 01:52:11 +0300

    update

--- High_Level_MC/text_manipulation/test.py
+++ High_Level_MC/text_manipulation/test.py
@@ -1,14 +1,4 @@
-#the string that will be saved as a .txt
-whole_text = '''weight = 10 kg 
-IR = "YES" 
-Xa = 0.5
-Ya = 0.1
-Za = 0.8
-Xg = 0.3
-Yg = 1.4
-Zg = 2.5
-Heading = 105 degrees'''
-
+#the values that will be put in the string
 values = [
     '10',       #weight 
     'YES',    #IR
@@ -21,6 +11,63 @@ values = [
     '105'       #Heading
 ]
 
+#this template to allow me to update the whole_text
+whole_text_template = '''weight = {} kg 
+IR = {} 
+Xa = {}
+Ya = {}
+Za = {}
+Xg = {}
+Yg = {}
+Zg = {}
+Heading = {} degrees'''
 
-while (1):
-    buffer_received = "10 kg"
+#the string that will be saved as a .txt
+whole_text = '''weight = {} kg 
+IR = {} 
+Xa = {}
+Ya = {}
+Za = {}
+Xg = {}
+Yg = {}
+Zg = {}
+Heading = {} degrees'''
+
+whole_text = whole_text.format(*values) #put the values in the string # Update Values
+
+#buffer received string (from Wifi)
+buffer_received = "weight = 110kg"
+
+#extract number from the buffer_received string
+number_in_buffer = ''.join(filter(str.isdigit, buffer_received))
+
+#if the buffer contains the break character, clear the buffer
+# -1 is the break character
+if "-1" in buffer_received:
+    buffer_received = ""
+
+if "weight" in buffer_received:
+    values[0] = number_in_buffer
+if "IR" in buffer_received:
+    values[1] = number_in_buffer
+if "Xa" in buffer_received:
+    values[2] = number_in_buffer
+if "Ya" in buffer_received:
+    values[3] = number_in_buffer
+if "Za" in buffer_received:
+    values[4] = number_in_buffer
+if "Xg" in buffer_received:
+    values[5] = number_in_buffer
+if "Yg" in buffer_received:
+    values[6] = number_in_buffer
+if "Zg" in buffer_received:
+    values[7] = number_in_buffer
+if "Heading" in buffer_received:
+    values[8] = number_in_buffer
+
+whole_text = whole_text.format(*values) #put the values in the string # Update Values
+
+print(whole_text) #print the string
+
+#while (1):
+    #buffer_received = "10 kg"

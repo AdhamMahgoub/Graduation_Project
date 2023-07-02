4a707a9227817a94fa00bf0cedae94b9a29d5b4f
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-07-03 02:04:34 +0300

    now it does what i want

--- High_Level_MC/text_manipulation/test.py
+++ High_Level_MC/text_manipulation/test.py
@@ -1,3 +1,5 @@
+import re
+
 #the values that will be put in the string
 values = [
     '10',       #weight 
@@ -63,11 +65,17 @@ while (1):
     buffer_keywords = ["weight", "IR", "Xa", "Ya", "Za", "Xg", "Yg", "Zg", "Heading"]
     for keyword in buffer_keywords:
         if keyword in buffer_received:
-            value = ''.join(filter(str.isdigit, buffer_received))
+            #value = ''.join(filter(str.isdigit, buffer_received))
+            #values[buffer_keywords.index(keyword)] = value
+            # Use regular expression to find numeric values (integers or floats)
+            value = re.search(r"[-+]?\d*\.\d+|\d+", buffer_received).group()
             values[buffer_keywords.index(keyword)] = value
             break  # No need to continue checking other keywords if a match is found
     
     # Format the whole_text with the updated values
     whole_text = whole_text_template.format(*values)
+
+
     # Print the updated string
+    # Send the whole_text string to a .txt file
     print(whole_text)

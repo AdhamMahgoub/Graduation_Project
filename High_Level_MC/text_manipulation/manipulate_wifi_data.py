121499246192f2e56bac5ddc22c63caeb2114d09
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-07-03 02:41:45 +0300

    added the tkinter to the manipulate_data file

--- High_Level_MC/text_manipulation/manipulate_wifi_data.py
+++ High_Level_MC/text_manipulation/manipulate_wifi_data.py
@@ -1,4 +1,4 @@
-import re
+import re #to be able to extract the numerical part
 
 #the values that will be put in the string
 values = [
@@ -79,3 +79,48 @@ while (1):
     # Print the updated string
     # Send the whole_text string to a .txt file to be read by the GUI
     print(whole_text)
+
+
+
+
+def display_gui():
+    import tkinter as tk
+    from tkinter import ttk
+
+    root = tk.Tk()
+    root.geometry("250x250")
+
+    # Create a style object
+    style = ttk.Style()
+
+    # Configure the style to use a solid background color and update label styles
+    style.configure(
+        "Custom.TFrame",
+        background="#F0F0F0",  # Set a solid background color
+    )
+
+    style.configure(
+        "Custom.TLabel",
+        font=("Arial", 14, "bold"),  # Update font style and size
+        foreground="#333333",  # Set label text color
+    )
+
+    # Create a frame using the custom style
+    frame = ttk.Frame(root, style="Custom.TFrame")
+    frame.pack(fill=tk.BOTH, expand=True)
+
+    # Read the variables from the text files
+        #product = product_file.read().strip()
+        #price = price_file.read().strip()
+        weight = values[0]
+
+    # Create the labels with the updated styles and variables
+    title_label = ttk.Label(frame, text="Smart Retail System", style="Custom.TLabel")
+    #product_label = ttk.Label(frame, text=f"Product: {product}", style="Custom.TLabel")
+    #price_label = ttk.Label(frame, text=f"Total Price: ${price}", style="Custom.TLabel")
+    weight_label = ttk.Label(frame, text=f"Weight: {weight}kg", style="Custom.TLabel")
+
+    title_label.pack(side="top", pady=10)
+    #product_label.pack(side="top", anchor="w")
+    weight_label.pack(anchor="w")
+    #price_label.pack(side="bottom", anchor="se")

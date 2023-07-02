dd2d73dd55f443af15f76fe6cbb5aaeb4065eb57
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-07-03 01:24:48 +0300

    update

--- High_Level_MC/text_manipulation/test.py
+++ High_Level_MC/text_manipulation/test.py
@@ -9,4 +9,18 @@ Yg = 1.4
 Zg = 2.5
 Heading = 105 degrees'''
 
-buffer_received = "10 kg"
+values = [
+    '10',       #weight 
+    'YES',    #IR
+    '0.5',      #Xa
+    '0.1',      #Ya
+    '0.8',      #Za
+    '0.3',      #Xg
+    '1.4',      #Yg
+    '2.5',      #Zg
+    '105'       #Heading
+]
+
+
+while (1):
+    buffer_received = "10 kg"

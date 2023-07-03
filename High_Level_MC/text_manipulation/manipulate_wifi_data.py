9792a800acaa2de8969bc642a258ae9ddb215bc5
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-07-03 13:52:05 +0300

    fully working GUI

--- High_Level_MC/text_manipulation/manipulate_wifi_data.py
+++ High_Level_MC/text_manipulation/manipulate_wifi_data.py
@@ -13,74 +13,6 @@ values = [
     '105'       #Heading
 ]
 
-def display_gui():
-    import tkinter as tk
-    from tkinter import ttk
-
-    root = tk.Tk()
-    root.geometry("250x250")
-
-    # Create a style object
-    style = ttk.Style()
-
-    # Configure the style to use a solid background color and update label styles
-    style.configure(
-        "Custom.TFrame",
-        background="#F0F0F0",  # Set a solid background color
-    )
-
-    style.configure(
-        "Custom.TLabel",
-        font=("Arial", 14, "bold"),  # Update font style and size
-        foreground="#333333",  # Set label text color
-    )
-
-    # Create a frame using the custom style
-    frame = ttk.Frame(root, style="Custom.TFrame")
-    frame.pack(fill=tk.BOTH, expand=True)
-
-    # Create the labels with the updated styles and initial values
-    weight_label = ttk.Label(frame, text="Weight: 0kg", style="Custom.TLabel")
-    ir_label = ttk.Label(frame, text="IR: N/A", style="Custom.TLabel")
-    xa_label = ttk.Label(frame, text="Xa: N/A", style="Custom.TLabel")
-    ya_label = ttk.Label(frame, text="Ya: N/A", style="Custom.TLabel")
-    za_label = ttk.Label(frame, text="Za: N/A", style="Custom.TLabel")
-    xg_label = ttk.Label(frame, text="Xg: N/A", style="Custom.TLabel")
-    yg_label = ttk.Label(frame, text="Yg: N/A", style="Custom.TLabel")
-    zg_label = ttk.Label(frame, text="Zg: N/A", style="Custom.TLabel")
-    heading_label = ttk.Label(frame, text="Heading: N/A degrees", style="Custom.TLabel")
-
-    # Pack the labels
-    weight_label.pack(anchor="w")
-    ir_label.pack(anchor="w")
-    xa_label.pack(anchor="w")
-    ya_label.pack(anchor="w")
-    za_label.pack(anchor="w")
-    xg_label.pack(anchor="w")
-    yg_label.pack(anchor="w")
-    zg_label.pack(anchor="w")
-    heading_label.pack(anchor="w")
-
-    # Function to update the labels with new values
-    def update_data():
-        weight, ir, xa, ya, za, xg, yg, zg, heading = map(float, values)
-        weight_label.config(text=f"Weight: {weight}kg")
-        ir_label.config(text=f"IR: {ir}")
-        xa_label.config(text=f"Xa: {xa}")
-        ya_label.config(text=f"Ya: {ya}")
-        za_label.config(text=f"Za: {za}")
-        xg_label.config(text=f"Xg: {xg}")
-        yg_label.config(text=f"Yg: {yg}")
-        zg_label.config(text=f"Zg: {zg}")
-        heading_label.config(text=f"Heading: {heading} degrees")
-
-    # Example of how to call the function and update the labels
-    update_data()
-
-
-
-
-
 #this template to allow me to update the whole_text
 whole_text_template = '''weight = {} kg 
 IR = {} 
@@ -120,6 +52,76 @@ whole_text = whole_text_template.format(*values)
 
 print(whole_text) #print the string
 
+
+########################    GUI init   ########################
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("250x250")
+
+# Create a style object
+style = ttk.Style()
+
+# Configure the style to use a solid background color and update label styles
+style.configure(
+    "Custom.TFrame",
+    background="#F0F0F0",  # Set a solid background color
+)
+
+style.configure(
+    "Custom.TLabel",
+    font=("Arial", 14, "bold"),  # Update font style and size
+    foreground="#333333",  # Set label text color
+)
+
+# Create a frame using the custom style
+frame = ttk.Frame(root, style="Custom.TFrame")
+frame.pack(fill=tk.BOTH, expand=True)
+
+# Create the labels with the updated styles and initial values
+title_label = ttk.Label(frame, text="Smart Retail System", style="Custom.TLabel")
+weight_label = ttk.Label(frame, text="Weight: 0kg", style="Custom.TLabel")
+ir_label = ttk.Label(frame, text="IR: N/A", style="Custom.TLabel")
+xa_label = ttk.Label(frame, text="Xa: N/A", style="Custom.TLabel")
+ya_label = ttk.Label(frame, text="Ya: N/A", style="Custom.TLabel")
+za_label = ttk.Label(frame, text="Za: N/A", style="Custom.TLabel")
+xg_label = ttk.Label(frame, text="Xg: N/A", style="Custom.TLabel")
+yg_label = ttk.Label(frame, text="Yg: N/A", style="Custom.TLabel")
+zg_label = ttk.Label(frame, text="Zg: N/A", style="Custom.TLabel")
+heading_label = ttk.Label(frame, text="Heading: N/A degrees", style="Custom.TLabel")
+
+# Pack the labels
+title_label.pack(side="top", pady=10)
+weight_label.pack(anchor="w")
+ir_label.pack(anchor="w")
+xa_label.pack(anchor="w")
+ya_label.pack(anchor="w")
+za_label.pack(anchor="w")
+xg_label.pack(anchor="w")
+yg_label.pack(anchor="w")
+zg_label.pack(anchor="w")
+heading_label.pack(anchor="w")
+
+# Function to update the labels with new values
+def update_data():
+    weight, ir, xa, ya, za, xg, yg, zg, heading = map(float, values)
+    weight_label.config(text=f"Weight: {weight}kg")
+    ir_label.config(text=f"IR: {ir}")
+    xa_label.config(text=f"Xa: {xa}")
+    ya_label.config(text=f"Ya: {ya}")
+    za_label.config(text=f"Za: {za}")
+    xg_label.config(text=f"Xg: {xg}")
+    yg_label.config(text=f"Yg: {yg}")
+    zg_label.config(text=f"Zg: {zg}")
+    heading_label.config(text=f"Heading: {heading} degrees")
+
+# Example of how to call the function and update the labels
+update_data()
+########################    End of GUI  ########################
+
+
+
 while (1):
     #buffer_received = "10 kg"
     #read the buffer_received from the user
@@ -146,9 +148,8 @@ while (1):
 
     # Print the updated string
     # Send the whole_text string to a .txt file to be read by the GUI
+    update_data()
     print(whole_text)
-    display_gui()
-
 
 
 

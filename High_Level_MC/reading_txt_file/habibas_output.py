abbd371633158a16c450b548ce93dc3e388a0d68
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-07-05 03:33:27 +0300

    added the read .txt file

---
+++ High_Level_MC/reading_txt_file/habibas_output.py
@@ -0,0 +1,19 @@
+def read_file_to_string(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            value = file.read()
+            return value
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+# Replace 'readme.txt' with the actual path to your file
+file_name = 'readme.txt'
+value = read_file_to_string(file_name)
+
+if value is not None:
+    print("File content:")
+    print(value)

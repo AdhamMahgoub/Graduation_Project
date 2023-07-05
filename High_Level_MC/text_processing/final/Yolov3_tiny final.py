0fa9e54aa432c1e8032ce40fcca0c68f172f0c4d
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-07-05 04:45:49 +0300

    updated YOLO

--- High_Level_MC/text_processing/final/Yolov3_tiny final.py
+++ High_Level_MC/text_processing/final/Yolov3_tiny final.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 # In[2]:
-max_pair = -1
+
 
 net=cv2.dnn.readNetFromDarknet("yolov3-tiny_custom.cfg","yolov3-tiny_custom_40000.weights")
 
@@ -54,6 +54,88 @@ colors= np.random.uniform(0,255,size=(len(classes),3))
 # 
 
 # In[7]:
+
+
+import time
+def detect_videos():
+    #For videos
+    #cap=cv2.VideoCapture('pepsi.webm')
+    #For Webcam
+    cap = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_PLAIN
+    starting_time= time.time()
+    frame_id = 0
+    
+    while True:
+        ret,frame= cap.read()
+        frame_id+=1
+        height,width,channels = frame.shape
+        #Detecting objects
+        blob = cv2.dnn.blobFromImage(frame,0.00392,(320,320),(0,0,0),True,crop=False) #reduce 416 to 320 
+        net.setInput(blob)
+        outs = net.forward(outputlayers)
+        #print(outs[1])
+        
+        #Showing info on screen/ get confidence score of algorithm in detecting an object in blob
+        class_ids=[]
+        confidences=[]
+        boxes=[]
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                
+                if confidence > 0.7:
+                    #onject detected
+                    center_x= int(detection[0]*width)
+                    center_y= int(detection[1]*height)
+                    w = int(detection[2]*width)
+                    h = int(detection[3]*height)
+                    
+                    #cv2.circle(img,(center_x,center_y),10,(0,255,0),2)
+                    #rectangle co-ordinaters
+                    x=int(center_x - w/2)
+                    y=int(center_y - h/2)
+                    #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+                    boxes.append([x,y,w,h]) #put all rectangle areas
+                    confidences.append(float(confidence)) #how confidence was that object detected and show that percentage
+                    class_ids.append(class_id) #name of the object tha was detected
+            
+        indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.4,0.6)
+        
+        for i in range(len(boxes)):
+            if i in indexes:
+                x,y,w,h = boxes[i]
+                label = str(classes[class_ids[i]])
+                confidence= confidences[i]
+                #color = colors[class_ids[i]]
+                color = (0,0,255)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
+                cv2.putText(frame,label+" "+str(round(confidence,2)),(x,y+30),font,1,(255,255,255),2)
+            
+        elapsed_time = time.time() - starting_time
+        fps=frame_id/elapsed_time
+        cv2.putText(frame,"FPS:"+str(round(fps,2)),(10,50),font,2,(0,0,255),1)
+    
+        cv2.imshow("Image",frame)
+        key = cv2.waitKey(1) #wait 1ms the loop will start again and we will process the next frame
+    
+        if key == 27: #esc key stops the process
+            break;        
+    cap.release()    
+    cv2.destroyAllWindows() 
+    return class_ids,confidences
+
+
+# In[10]:
+
+
+detect_videos()
+
+
+# In[11]:
 
 
 def convert_id_to_name(class_id):
@@ -134,106 +216,6 @@ def convert_id_to_name(class_id):
     elif class_id==37:
         return 'Pepsi'
 
-import time
-def detect_videos():
-    #For videos
-    #cap=cv2.VideoCapture('pepsi.webm')
-    #For Webcam
-    cap = cv2.VideoCapture(0)
-    font = cv2.FONT_HERSHEY_PLAIN
-    starting_time= time.time()
-    frame_id = 0
-    
-    while True:
-        ret,frame= cap.read()
-        frame_id+=1
-        height,width,channels = frame.shape
-        #Detecting objects
-        blob = cv2.dnn.blobFromImage(frame,0.00392,(320,320),(0,0,0),True,crop=False) #reduce 416 to 320 
-        net.setInput(blob)
-        outs = net.forward(outputlayers)
-        #print(outs[1])
-        
-        #Showing info on screen/ get confidence score of algorithm in detecting an object in blob
-        class_ids=[]
-        confidences=[]
-        boxes=[]
-        for out in outs:
-            for detection in out:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
-                
-                if confidence > 0.7:
-                    #onject detected
-                    center_x= int(detection[0]*width)
-                    center_y= int(detection[1]*height)
-                    w = int(detection[2]*width)
-                    h = int(detection[3]*height)
-                    
-                    #cv2.circle(img,(center_x,center_y),10,(0,255,0),2)
-                    #rectangle co-ordinaters
-                    x=int(center_x - w/2)
-                    y=int(center_y - h/2)
-                    #cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-
-                    boxes.append([x,y,w,h]) #put all rectangle areas
-                    confidences.append(float(confidence)) #how confidence was that object detected and show that percentage
-                    class_ids.append(class_id) #name of the object tha was detected
-            
-        indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.4,0.6)
-        
-        for i in range(len(boxes)):
-            if i in indexes:
-                x,y,w,h = boxes[i]
-                label = str(classes[class_ids[i]])
-                confidence= confidences[i]
-                #color = colors[class_ids[i]]
-                color = (0,0,255)
-                cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
-                cv2.putText(frame,label+" "+str(round(confidence,2)),(x,y+30),font,1,(255,255,255),2)
-            
-        elapsed_time = time.time() - starting_time
-        fps=frame_id/elapsed_time
-        cv2.putText(frame,"FPS:"+str(round(fps,2)),(10,50),font,2,(0,0,255),1)
-    
-        cv2.imshow("Image",frame)
-        key = cv2.waitKey(1) #wait 1ms the loop will start again and we will process the next frame
-    
-        def save_to_text(textfile_path):
-            result = max_pair
-            # Open a file in write mode
-            file = open(textfile_path, "w")
-            # Convert each item to a string and write it to the file
-            #for item in pair:
-            item=convert_id_to_name(result[0])
-            confidence_score=result[1]
-            file.write(str(item) + "\t" + str(price(result[0])))
-            # Close the file    
-            file.close()
-            
-        # In[23]:
-        save_to_text('Detection.txt')
-
-
-        if key == 27: #esc key stops the process
-            break;        
-    cap.release()    
-    cv2.destroyAllWindows() 
-    return class_ids,confidences
-
-
-# In[10]:
-
-
-detect_videos()
-
-
-# In[11]:
-
-
-
-
 
 # In[18]:
 
@@ -288,5 +270,21 @@ print("Pair with the maximum confidence score:", max_pair)
 # In[22]:
 
 
+def save_to_text(textfile_path):
+    result = max_pair
+    # Open a file in write mode
+    file = open(textfile_path, "w")
+    # Convert each item to a string and write it to the file
+    #for item in pair:
+    item=convert_id_to_name(result[0])
+    confidence_score=result[1]
+    file.write(str(item) + "\t" + str(price(result[0])))
+    # Close the file    
+    file.close()
 
+
+# In[23]:
+
+
+save_to_text('Detection.txt')
 
